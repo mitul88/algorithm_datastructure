ecbed331e927626a1caf6e2145fed335413dc7c0
mitul88 <shahed.rahman88@gmail.com>
2024-07-09 14:53:49 +0600

    assignment redo done

--- shuffledString.js
+++ shuffledString.js
@@ -1,20 +1,16 @@
-const shuffleStr = function (s, idxs) {
-  if (s.length != idxs.length) {
-    return false;
+function shuffleStr(str, indices) {
+  var output = "";
+
+  for (let index = 0; index < indices.length; index++) {
+    output += str[indices[index]];
   }
-  let splitStr = s.split("");
-
-  let strObj = {};
-
-  for (let i = 0; i < idxs.length; i++) {
-    strObj[idxs[i]] = splitStr[i];
-  }
-
-  let sortedStrArr = Object.entries(strObj)
-    .sort((a, b) => a[1] - b[1])
-    .map((el) => el[1]);
-  return sortedStrArr.join("");
-};
+  return output;
+}
 
 console.log(shuffleStr("mamacode", [4, 5, 6, 7, 0, 1, 2, 3]));
-console.log(shuffleStr("dosta", [4, 0, 1, 2, 3]));
+console.log(shuffleStr("dosta", [1, 2, 3, 4, 0]));
+console.log(shuffleStr("carrace", [4, 5, 6, 0, 1, 2, 3]));
+
+// The above code has linear complexity for both time and space. We have nth steps and nth time value has been stored inside the "output" variable
+
+// NOTE: There is a mistake in the example number 2 which was given in the question.
